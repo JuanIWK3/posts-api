ac2243a2c412eaf93b7620f184eff253a67905e1
juaniwk3 <juaniwk3@gmail.com>
2022-10-21 23:22:43 -0300

    feat: create posts

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
+posts = []
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello eitititipi"}
+
+@app.get("/posts")
+def get_posts():
+    return posts 
+
+@app.post("/create")
+def create_posts(payload: dict = Body(...)):
+    posts.append({
+        "id": len(posts) + 1,
+        "title": payload["title"],
+        "content": payload["content"]
+    })
+    return {
+        "status": "created",
+        "post": {
+            "id": len(posts),
+            "title": payload["title"],
+            "content": payload["content"]
+        }
+    }
